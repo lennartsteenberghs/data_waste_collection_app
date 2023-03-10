f4a5a3768071429573645a022a429cb42c242156
ThiersTech <michiel.thiers@hotmail.com>
2023-03-10 15:05:02 -0400

    improved UI thankyou page and added gif

--- frontend/src/layouts/ThankYouLayout.vue
+++ frontend/src/layouts/ThankYouLayout.vue
@@ -1,31 +1,41 @@
 <template>
   <q-layout class="flex" view="lHh Lpr lFf">
     <q-header elevated>
-      <div class="text-center text-4xl pt-5 pb-2 bg-green">Let's collect data!</div>
+      <q-toolbar class="px-5 pt-5 pb-2 bg-space-cadet">
+        <q-avatar square>
+          <img class="" :src="require('../assets/aruba.png')" />
+        </q-avatar>
+
+        <q-toolbar-title class="text-center text-2xl"
+          >Let's collect data!</q-toolbar-title
+        >
+        <q-avatar square>
+          <img class="" :src="require('../assets/aruba.png')" />
+        </q-avatar>
+      </q-toolbar>
     </q-header>
-    <q-page-container class="flex-1 justify-between">
-      <div>
-        <div class="px-2 text-center text-3xl pt-10 pb-0">
+    <q-page-container class="flex-1 justify-between p-5">
+      <div class="">
+        <div v-if="!amount" class="px-2 text-center text-3xl pt-10 pb-6">
           Thank you for registering your waste!
         </div>
-        <div v-if="amount" class="px-5 text-center text-lg pt-1 pb-0">
+        <div v-else class="px-2 text-center text-3xl pt-10 pb-6">
           This recycle bin already saved {{ amount }} kg of CO2!
         </div>
-        <div v-else class="px-5 text-center text-lg pt-1 pb-0">
-          Together we can make Aruba a cleaner place
-        </div>
-        <div class="px-8">
+        <div class="">
           <iframe
             src="https://giphy.com/embed/iGMmoO25hxxUFRKfu4"
             width="full"
             height="full"
             frameBorder="0"
-            class="giphy-embed"
+            class="giphy-embed mx-auto"
             allowFullScreen
           ></iframe>
         </div>
       </div>
-      <div class="border rounded-2xl m-5 py-3 px-2 shadow-md fixed bottom-10">
+      <div
+        class="border w-5/6 max-w-sm rounded-2xl py-3 px-2 shadow-md mx-auto fixed bottom-5 left-1/2 transform -translate-x-1/2"
+      >
         <div class="text-center text-xl pt-0 pb-0">
           Would you be so kind to give some feedback on the app?
         </div>
