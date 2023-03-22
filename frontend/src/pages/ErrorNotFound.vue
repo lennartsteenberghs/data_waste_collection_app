0ba808624f51bc4ce861301cef490404616e0ec2
ThiersTech <michiel.thiers@hotmail.com>
2023-03-22 18:08:33 -0400

    changed 404 page

--- frontend/src/pages/ErrorNotFound.vue
+++ frontend/src/pages/ErrorNotFound.vue
@@ -1,31 +1,21 @@
 <template>
-  <div class="fullscreen bg-blue text-white text-center q-pa-md flex flex-center">
-    <div>
-      <div style="font-size: 30vh">
-        404
-      </div>
-
-      <div class="text-h2" style="opacity:.4">
-        Oops. Nothing here...
-      </div>
-
-      <q-btn
-        class="q-mt-xl"
-        color="white"
-        text-color="blue"
-        unelevated
-        to="/"
-        label="Go Home"
-        no-caps
-      />
+  <div class="fullscreen text-center p-4 flex flex-center">
+    <div class="text-3xl">Sorry, we couldn't find the page you're looking for</div>
+    <div class="text-xl">
+      Just like we couldn't find our friend Juan who has disappeared underneath all the
+      trash
+    </div>
+    <img class="p-1 h-60" :src="require('../assets/landfill.jpg')" />
+    <div class="text-xl pb-10">
+      Help us find Juan by recycling your waste and reducing Aruba's landfill!
     </div>
   </div>
 </template>
 
 <script>
-import { defineComponent } from 'vue'
+import { defineComponent } from "vue";
 
 export default defineComponent({
-  name: 'ErrorNotFound'
-})
+  name: "ErrorNotFound",
+});
 </script>
