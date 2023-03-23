742a0b15143b7910460c41661e007b24e1d3b53a
ThiersTech <michiel.thiers@hotmail.com>
2023-03-23 10:55:27 -0400

    updated css 404 page

--- frontend/src/pages/ErrorNotFound.vue
+++ frontend/src/pages/ErrorNotFound.vue
@@ -1,11 +1,13 @@
 <template>
-  <div class="fullscreen text-center p-4 flex flex-center">
-    <div class="text-3xl">Sorry, we couldn't find the page you're looking for</div>
+  <div class="fullscreen text-center p-4 flex-col items-center">
+    <div class="text-3xl pt-5 pb-2">
+      Sorry, we couldn't find the page you're looking for
+    </div>
     <div class="text-xl">
       Just like we couldn't find our friend Juan who has disappeared underneath all the
       trash
     </div>
-    <img class="p-1 h-60" :src="require('../assets/landfill.jpg')" />
+    <img class="my-10 h-60 mx-auto" :src="require('../assets/landfill.jpg')" />
     <div class="text-xl pb-10">
       Help us find Juan by recycling your waste and reducing Aruba's landfill!
     </div>
