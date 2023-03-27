3cfa0882d64197baa50013c69c3d0ec061774af9
lennart.steenberghs <lennart.steenberghs@student.kuleuven.be>
2023-03-27 16:43:37 -0400

    Added connection to S3 bucket

--- backend/models/waste_model.js
+++ backend/models/waste_model.js
@@ -1,16 +1,16 @@
 //import connection
-import pool from "../config/database.js";
+import MYSQL_pool from "../config/MYSQL_DB.js";
 
 
 //insert waste data to database
 export async function insertWaste(data, idbin) {
-  await pool.query("INSERT INTO waste (id_type, amount, id_bin) VALUES (?, ?, ?)",
+  await MYSQL_pool.query("INSERT INTO waste (id_type, amount, id_bin) VALUES (?, ?, ?)",
   [data.plastic_type, data.amount, idbin])
 }
 
 //Calculate CO2_safed
 export async function calculate_CO2_safed(idbin){
-  const mot_fact = await pool.query("call railway.calculate_CO2(?)", [idbin])
+  const mot_fact = await MYSQL_pool.query("call railway.calculate_CO2(?)", [idbin])
   return mot_fact[0][0][0]
 }
 
