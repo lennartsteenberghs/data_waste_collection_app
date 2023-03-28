3518c1531e456140546f5dfa03b82f2458f23497
ThiersTech <michiel.thiers@hotmail.com>
2023-03-28 15:36:16 -0400

    changed build to pwa

--- frontend/src-pwa/custom-service-worker.js
+++ frontend/src-pwa/custom-service-worker.js
@@ -8,3 +8,48 @@ import { precacheAndRoute } from 'workbox-precaching'
 
 // Use with precache injection
 precacheAndRoute(self.__WB_MANIFEST)
+
+
+// background sync to put API requests in Queue
+
+let createPostQueue = null
+if (backgroundSyncSupported) {
+  createPostQueue = new Queue('createPostQueue', {
+    onSync: async ({queue}) => {
+      let entry;
+      while (entry = await queue.shiftRequest()) {
+        try {
+          await fetch(entry.request);
+          console.log('Replay succesful for request', entry.request);
+          const clients = await self.clients.matchAll({type: 'window'});
+          for (const client of clients) {
+            client.postMessage({
+              msg: "offline-post-uploaded"
+            })
+          }
+        } catch (error) {
+          console.error('Replay failed for request', entry.request, error);
+
+          //put entry back in the queue and re-throw the error:
+          await queue.unshiftRequest(entry);
+          throw error
+        }
+      }
+    }
+  })
+}
+
+if(backgroundSyncSupported){
+  self.addEventListener('fetch', (event) => {
+    if (event.request.url == 'https://eenurl/createPost' || event.request.url == 'localhost:3000/createPost') {
+      // clone the request to ensure it's safe to read when adding to Queue
+      if(!self.navigator.onLine) {
+        const promiseChain = fetch(event.request.clone()).catch((err) => {
+          return createPostQueue.pushRequest({request: event.request});
+        });
+
+        event.waitUntil(promiseChain);
+      }
+    }
+  });
+}
---
+++ frontend/src/composables/postWasteData copy.js
@@ -0,0 +1,43 @@
+import { ref } from "vue";
+
+const postWasteData = () => {
+
+    const postDataError = ref(null);
+
+    const post = async (list, binId) => {
+        const body = turnIntoJSON(list)
+        const url = process.env.API_POST_URL + binId
+        console.log("posting data to: ",url)
+
+        const requestOptions = {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: body
+        };
+
+        try {
+            let response = await fetch(url, requestOptions);
+            if (!response.ok) {
+                throw Error("post failed");
+            }
+            console.log("post succesful")
+        } catch (err) {
+            postDataError.value = err.message;
+            console.log(postDataError.value);
+        }
+    }
+
+    const turnIntoJSON = (list) => {
+        const newList = []
+        for (let i = 0; i < list.length; i++) {
+            console.log(list[i])
+            const item = { 'plastic_type': list[i].id, 'amount': list[i].count}
+            newList.push(item)
+        }
+        const jsonOutput = JSON.stringify(newList)
+        return jsonOutput
+    }
+
+    return { postDataError: postDataError, post }
+}
+export default postWasteData
